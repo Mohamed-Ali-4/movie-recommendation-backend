9efe587806fd5ba7df23cacaab1028121614fa21
Mohamed Ali <mohamedali04123@gmail.com>
2026-05-14 22:52:45 +0530

    Fix UserProfile mongoose overwrite error

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -7,14 +7,17 @@ const UserProfileSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
+
   favorites: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Movie'
   }],
+
   watchlist: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Movie'
   }],
+
   watched: [{
     movie: {
       type: mongoose.Schema.Types.ObjectId,
@@ -23,18 +26,28 @@ const UserProfileSchema = new mongoose.Schema({
     rating: Number,
     watchedAt: Date
   }],
+
   moodPreferences: {
     type: [String],
     default: []
   },
+
   recentSearches: {
     type: [String],
     default: []
   },
+
   recommendationHistory: [{
     movies: [mongoose.Schema.Types.ObjectId],
-    createdAt: { type: Date, default: Date.now }
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
   }]
+
 }, { timestamps: true });
 
-module.exports = mongoose.model('UserProfile', UserProfileSchema);
+// FIX OVERWRITE MODEL ERROR
+module.exports =
+  mongoose.models.UserProfile ||
+  mongoose.model('UserProfile', UserProfileSchema);
